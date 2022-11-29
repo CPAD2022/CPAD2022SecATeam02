8524c215a4ecd6f19580310ec68d4894ef7feadd
UpendraMallya <58103652+Wilton-Dsouza@users.noreply.github.com>
2022-11-29 13:06:47 +0530

    Updated Structure with Firebase and Added common widgets
    Added the folder structure of the projects and also add common widgets which contains customized buttons, loading, text fields and container

--- android/app/src/main/kotlin/com/example/perfect_fitness/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.perfect_fitness
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/core/const/global_constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:perfect_fitness/data/user_data.dart';
-import 'package:perfect_fitness/data/workout_data.dart';
-
-class GlobalConstants {
-  static UserData? currentUser;
-  static List<WorkoutData>? workouts;
-}
